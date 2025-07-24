de5375783b060b9fbb72ed4cd4e2c9590f67efff
Dylan Beattie <dylan@dylanbeattie.net>
2025-07-25 00:01:17 +0100

    vault backup: 2025-07-25 00:01:17
    Affected files:

--- _plugins/example.rb
+++ _plugins/example.rb
@@ -5,12 +5,13 @@ module Jekyll
     class ExampleTag < Liquid::Tag
       include Liquid::StandardFilters
 
-      # The regular expression syntax checker. Start with the language specifier.
-      # Follow that by zero or more space separated options that take one of three
-      # forms: name, name=value, or name="<quoted list>"
-      #
-      # <quoted list> is a space-separated list of numbers
       SYNTAX = %r!^([a-zA-Z0-9.+#_-]+)((\s+\w+(=(\w+|"([0-9]+\s)*[0-9]+"))?)*)$!.freeze
+
+				def initialize(_tag_name, markup, _parse_context)
+	  super
+	  @markup = markup.strip
+	end
+			
 
       def initialize(tag_name, markup, tokens)
         super
