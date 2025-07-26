366d06134fc5b3ad41ade9ab89bebc2e605c9ca3
Dylan Beattie <dylan@dylanbeattie.net>
2025-07-26 23:53:39 +0100

    vault backup: 2025-07-26 23:53:39
    Affected files:

--- rebuild-sections.ps1
+++ rebuild-sections.ps1
@@ -4,8 +4,8 @@
 $oldPartNumber = 0
 
 Get-ChildItem -Path . -Filter *.md | Sort-Object Name | ForEach-Object {
-	$oldFilename = $_.Name	
-	if ($oldFilename -match '^(\d+)-(\d+)[a-z]?-(.*)\.md$') {
+	$oldFilename = $_.BaseName
+	if ($oldFilename -match '^(\d+)-(\d+)[a-z]?-(.*)$') {
 		$newPartNumber = $matches[1]
 		if ($newPartNumber -eq $oldPartNumber) {
 			$newSectionNumber = $newSectionNumber + 1
@@ -15,40 +15,13 @@ Get-ChildItem -Path . -Filter *.md | Sort-Object Name | ForEach-Object {
 		}
 		$title = $matches[3]
 		$newFileName = "{0}{1:00}-$title.md" -f [int]$newPartNumber, [int]$newSectionNumber
-		Rename-Item -Path $oldFilename -NewName $newFileName
-		Rename-Item -Path "examples\$oldFilename" -NewName "examples\$newFileName"
-		Write-Host "$oldFileName > $newFileName"
+		Rename-Item -Path "$oldFilename.md" -NewName $newFileName
+
+		if (Test-Path "examples\$oldFilename") {
+			Rename-Item -Path "examples\$oldFilename" -NewName "examples\$newFileName" -Force
+		}
+		Write-Host "$oldFileName.md > $newFileName"
 	} else {
 		Write-Host "Filename does not match expected pattern: $($_.Name)"
 	}
-	# $file = $_.FullName
-	# $content = Get-Content $file -Raw
-	# if ($content -match "(?s)^---\s*(.*?)\s*---\s*(.*)") {
-	# 	$frontMatter = $matches[1]
-	# 	$body = $matches[2]
-	# 	$wordCount = ($body -split '\s+') 
-	# 		| Where-Object { $_ -match '\w' } 
-	# 		| Measure-Object 
-	# 		| Select-Object -ExpandProperty Count
-
-	# 	if ($frontMatter -match "word_count:\s*\d+") {
-	# 		$newFrontMatter = $frontMatter -replace "word_count:\s*\d+", "word_count: $wordCount"
-	# 	} else {
-	# 		$newFrontMatter = $frontMatter + "`nword_count: $wordCount"
-	# 	}
-	# 	$totalWordCount += $wordCount
-	# 	$newContent = "---`r`n$newFrontMatter`r`n---`r`n$body"
-	# 	Set-Content -NoNewline -Path $file -Value $newContent
-	# 	Write-Host "Updated word count for $($_.Name)"
-	# } else {
-	# 	Write-Host "No front matter found in $($_.Name)"
-	# }
 }
-
-# Write-Host "Total word count: $totalWordCount"
-# Write-Host "Total duration: $($totalWordCount / 200) minutes"
-
-# git add .
-# git commit -m "Word count: $totalWordCount"
-
-
