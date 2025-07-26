ed7574a0f51f7a1f3c8f3a93c646f8b01f42e1fa
Dylan Beattie <dylan@dylanbeattie.net>
2025-07-27 00:13:48 +0100

    Snapshot

--- update-nav-order.ps1
+++ update-nav-order.ps1
@@ -1,6 +1,7 @@
 Get-ChildItem -Path . -Filter *.md | ForEach-Object {
 	if ($_.Name -match '^(\d+)-(.*)$') {
 		$filePath = $_.FullName
+		$baseName = $_.BaseName
 		$navOrder = $matches[1]
 		$content = Get-Content $filePath -Raw
 		if ($content -match "(?s)^---\s*(.*?)\s*---\s*(.*)") {
@@ -8,13 +9,21 @@ Get-ChildItem -Path . -Filter *.md | ForEach-Object {
 			$body = $matches[2]
 			if ($frontMatter -match "nav_order:\s*\d+") {
 				$newFrontMatter = $frontMatter -replace "nav_order:\s*\d+", "nav_order: $navOrder"
-			}	else {
+			}
+			else {
 				$newFrontMatter = $frontMatter + "`nav_order: $navOrder"
+			}
+			if ($newFrontMatter -match "examples: [^ ]+") {
+				$newFrontMatter = $newFrontMatter -replace "examples: [^ ]+", "examples: examples/$baseName"
+			}
+			else {
+				$newFrontMatter = $newFrontMatter + "`nexamples: examples/$baseName"
 			}
 			$newContent = "---`r`n$newFrontMatter`r`n---`r`n$body"
 			Set-Content -NoNewline -Path $filePath -Value $newContent
 			Write-Host "Updated nav_order for $filePath to $navOrder"
-		}	else {
+		}
+		else {
 			Write-Host "No front matter found in $($_.Name)"
 		}
 	}
