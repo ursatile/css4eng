70a82dd328aef12ff0b30946844f7d5578440dbc
Dylan Beattie <dylan@dylanbeattie.net>
2025-07-26 23:25:39 +0100

    vault backup: 2025-07-26 23:25:39
    Affected files:

---
+++ rebuild-sections.ps1
@@ -0,0 +1,36 @@
+# git add .
+# git commit -m "Snapshot of everything before running rebuild-sections.ps1"
+
+Get-ChildItem -Path . -Filter *.md | Sort-Object Name | ForEach-Object {
+	Write-Host $_
+	# $file = $_.FullName
+	# $content = Get-Content $file -Raw
+	# if ($content -match "(?s)^---\s*(.*?)\s*---\s*(.*)") {
+	# 	$frontMatter = $matches[1]
+	# 	$body = $matches[2]
+	# 	$wordCount = ($body -split '\s+') 
+	# 		| Where-Object { $_ -match '\w' } 
+	# 		| Measure-Object 
+	# 		| Select-Object -ExpandProperty Count
+
+	# 	if ($frontMatter -match "word_count:\s*\d+") {
+	# 		$newFrontMatter = $frontMatter -replace "word_count:\s*\d+", "word_count: $wordCount"
+	# 	} else {
+	# 		$newFrontMatter = $frontMatter + "`nword_count: $wordCount"
+	# 	}
+	# 	$totalWordCount += $wordCount
+	# 	$newContent = "---`r`n$newFrontMatter`r`n---`r`n$body"
+	# 	Set-Content -NoNewline -Path $file -Value $newContent
+	# 	Write-Host "Updated word count for $($_.Name)"
+	# } else {
+	# 	Write-Host "No front matter found in $($_.Name)"
+	# }
+}
+
+# Write-Host "Total word count: $totalWordCount"
+# Write-Host "Total duration: $($totalWordCount / 200) minutes"
+
+# git add .
+# git commit -m "Word count: $totalWordCount"
+
+
